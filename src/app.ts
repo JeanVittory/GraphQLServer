46106e0f6fcb67f67576bad992698f095f969f92
JeanVittory <vittory.dev@gmail.com>
2023-01-04 18:19:06 -0500

    adding cors

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
+import cors from 'cors';
 import express from 'express';
 import { graphqlHTTP } from 'express-graphql';
 import { schema } from './graphql/schema';
 
 const app = express();
+
+app.use(cors());
 
 app.use(
 	'/graphql',
